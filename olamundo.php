f9edecb45caa12cbb825ac10d128a54f4b23b1b3
aline2003-php <202603539962@alunos.estacio.br>
2026-03-25 18:39:33 -0400

    Clean up olamundo.php and add greeting

---
+++ olamundo.php
@@ -0,0 +1,27 @@
+<?php
+print "Olá, mundo!";
+?>
+
+//Tipos de dados em PHP
+
+<?php
+// Tipo FLOAT (número decimal)
+$v = 300.5;
+var_dump($v); 
+// Resultado: float(300.5)
+
+// Tipo INTEGER (número inteiro)
+$v = 300;
+var_dump($v); 
+// Resultado: int(300)
+
+// Tipo BOOLEAN (verdadeiro/falso)
+$v = true;
+var_dump($v); 
+// Resultado: bool(true)
+
+// Tipo STRING (texto)
+$v = "Aline";
+var_dump($v); 
+// Resultado: string(5) "Aline"
+?>
